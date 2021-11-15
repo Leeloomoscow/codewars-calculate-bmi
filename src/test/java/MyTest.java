2319f814dfcf4f1a7f170ac0febfc32cd54c900e
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-15 20:49:01 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,31 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testBMI() {
+        assertEquals("Normal", Calculate.bmi(80, 1.80));
+    }
 
+    private static class Calculate {
+        public static String bmi(double weight, double height) {
+            String category = "";
+            double BMI = weight / (height * height);
+            if (BMI <= 18.5) {
+                return "Underweight";
+            }
+            if (BMI <= 25.0) {
+                return "Normal";
+            }
+            if (BMI <= 30.0) {
+                return "Overweight";
+            }
+            if (BMI > 30) {
+                return "Obese";
+            }
+            return category;
+        }
     }
 }
+
